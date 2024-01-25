95c6a147ac08138f0962303fd47a19f43ab9fa92
Felix Kimbu <62715603+MOLeCULE-Soft@users.noreply.github.com>
2024-01-25 13:39:20 +0100

    Update quizer.js

--- quizer.js
+++ quizer.js
@@ -1,7 +1,7 @@
 /**
- * ALX Quiz Checker - Helps avoid over scrolling during quizes
+ * ALX Quiz Checker - Helps avoid over-scrolling during quizes
  * @author: Felix Kimbu <felixkimbu@outlook.com>
- * @version: 2024.01.24.12.20
+ * @version: 1.0.0
  */
 
 /* Get quiz submit button */
@@ -16,50 +16,48 @@ let oldErrorCount = 0;
 /**
  * check - Provide quiz state / result
  */
-function check(){
-    /* Get number of errors after submission */
-    newErrorCount = parseInt(document.getElementsByClassName("error")[0].innerHTML.split(" ")[0]);
-    
-    let errorDifference = newErrorCount - oldErrorCount;
-    
-    console.log("After checking at", Date().toLocaleString(), "...");
-    if (errorDifference < 0)
-        console.log("You Passed:", Math.abs(errorDifference), "questions this time.");
-    else if (errorDifference > 0)
-        console.log("You Failed:", errorDifference, "questions this time.");
-    else
-        console.log("No change!");
-    
-    let done = questionCount - newErrorCount;
-    console.log("Completed quiz questions:", done, "/", questionCount);
-    oldErrorCount = newErrorCount;
+function check() {
+	/* Get number of errors after submission */
+	newErrorCount = parseInt(document.getElementsByClassName("error")[0].innerHTML.split(" ")[0]);
+
+	let errorDifference = newErrorCount - oldErrorCount;
+
+	console.log("After checking at", Date().toLocaleString(), "...");
+	if (errorDifference < 0)
+		console.log("You Passed:", Math.abs(errorDifference), "questions this time.");
+	else if (errorDifference > 0)
+		console.log("You Failed:", errorDifference, "questions this time.");
+	else
+		console.log("No change!");
+
+	let done = questionCount - newErrorCount;
+	console.log("Completed quiz questions:", done, "/", questionCount);
+	oldErrorCount = newErrorCount;
 }
 
-try
-{
-    if (observer);
-} catch (ReferenceError)
-{
-    console.log("Creating observer...");
-    /* MutationObserver to monitor the submit button
-     * i.e. check if the button has re-appeared
-     * (display changed from none to something else)
-     * N.B: This is important because the check is an asynchronous
-     * operation with an undefined time.
-     */
-    observer = new MutationObserver((mutations) => {
-      for (let mutation of mutations) {
-        if (mutation.attributeName === 'style') {
-          let display = submitBtn.style.display;
-          if (display !== 'none') {
-            check();
-          }
-        }
-      }
-    });
-    /* Register the submit button with the observer */
-    observer.observe(submitBtn, {
-      attributes: true, // observe attribute changes
-      attributeFilter: ['style'] // only observe style changes
-    });
+try { // check if observer is registered already
+	if (observer);
+} catch (ReferenceError) { // create and register observer
+	/* MutationObserver to monitor the submit button
+	 * i.e. check if the button has re-appeared
+	 * (display changed from none to something else)
+	 * N.B: This is important because the check is an asynchronous
+	 * operation with an undefined time.
+	 */
+	observer = new MutationObserver((mutations) => {
+		for (let mutation of mutations) {
+			if (mutation.attributeName === 'style') {
+				let display = submitBtn.style.display;
+				if (display !== 'none') {
+					check();
+				}
+			}
+		}
+	});
+
+	/* Register the submit button with the observer */
+	observer.observe(submitBtn, {
+		attributes: true, // observe attribute changes
+		attributeFilter: ['style'] // only observe style changes
+	});
 }
